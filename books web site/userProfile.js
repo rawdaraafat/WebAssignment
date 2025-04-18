924df9cf8763194e35d9e66ff1a8062fb3ce5c15
bob231323 <162380035+bob231323@users.noreply.github.com>
2025-04-18 18:17:14 +0200

    Update userProfile.js

--- books web site/userProfile.js
+++ books web site/userProfile.js
@@ -14,26 +14,13 @@ function updateProfilePage() {
   document.querySelector('p:nth-of-type(1)').innerHTML = `<strong>Age:</strong> ${userData.age}`;
   document.querySelector('p:nth-of-type(2)').innerHTML = `<strong>Location:</strong> ${userData.location}`;
   document.querySelector('p:nth-of-type(3)').innerHTML = `<strong>Hobbies:</strong> ${userData.hobbies}`;
-  document.querySelector('p:nth-of-type(4)').innerHTML = `<strong>Favourite book ever:</strong> ${userData.favoriteBook}`;
-  document.querySelector('p:nth-of-type(5)').innerHTML = `<strong>Profile password:</strong> ********`;
-  document.querySelector('p:nth-of-type(6)').innerHTML = `<strong>Card password:</strong> ********`;
-  document.querySelector('p:nth-of-type(7)').innerHTML = `<strong>Card number:</strong> ${userData.cardNumber || '4001 1253 **** ****'}`;
+  document.querySelector('p:nth-of-type(4)').innerHTML = `<strong>Profile password:</strong> ********`;
+  document.querySelector('p:nth-of-type(5)').innerHTML = `<strong>Card password:</strong> ********`;
+  //document.querySelector('p:nth-of-type(4)').innerHTML = `<strong>Profile password:</strong> ${userData.profilePassword}`;
+  //document.querySelector('p:nth-of-type(5)').innerHTML = `<strong>Card password:</strong> ${userData.cardPassword}`;
+  document.querySelector('p:nth-of-type(6)').innerHTML = `<strong>Card number:</strong> ${'4001 1253 **** ****'}`;
 
-  // Update lists
-  const borrowedList = document.querySelector('ul:nth-of-type(1)');
-  const favoriteList = document.querySelector('ul:nth-of-type(2)');
-  const dislikedList = document.querySelector('ul:nth-of-type(3)');
-
-  if (borrowedList) {
-    borrowedList.innerHTML = userData.borrowedBooks.map(book => `<li>${book}</li>`).join('');
-  }
-  if (favoriteList) {
-    favoriteList.innerHTML = userData.favoriteBooks.map(book => `<li>${book}</li>`).join('');
-  }
-  if (dislikedList) {
-    dislikedList.innerHTML = userData.dislikedBooks.map(book => `<li>${book}</li>`).join('');
-  }
 }
 
 // Update the profile when the page loads
-document.addEventListener('DOMContentLoaded', updateProfilePage); 
+document.addEventListener('DOMContentLoaded', updateProfilePage);
