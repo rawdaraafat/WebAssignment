f59404c1dc79abdc1056ef7e2f64d9537186ec27
rawdaraafat <144979504+rawdaraafat@users.noreply.github.com>
2025-05-14 01:36:00 +0300

    Update settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')        
+MEDIA_ROOT = os.path.join(BASE_DIR, 'main/media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -130,3 +130,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
     
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
