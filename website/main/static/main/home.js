aec8899b32460133d795cba4f2b606af5dc7f35f
rawdaraafat <rawdaramadan55@gmail.com>
2025-05-14 21:10:19 +0300

    BackEnd Project

---
+++ website/main/static/main/home.js
@@ -0,0 +1,26 @@
+const shelf = document.getElementById("bookShelf");
+
+function createBookCard(book) {
+  return `
+    <div class="book">
+      <div class="book-inner">
+        <div class="book-front">
+          <img src="${book.image}" alt="${book.title}">
+        </div>
+        <div class="book-back">
+          <p class="book-title">${book.title}</p>
+          <p class="book-author">${book.author}</p>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function loadAllBooks() {
+  // shelf.innerHTML = books.map(createBookCard).join("");
+  if (localStorage.getItem('books') == null) {
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+
+document.addEventListener("DOMContentLoaded", loadAllBooks);
